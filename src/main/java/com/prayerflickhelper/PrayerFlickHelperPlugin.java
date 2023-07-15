f380e382c81aad486a529d534647a61cb6bf6de7
ryanYtan <tanyuryan@gmail.com>
2023-07-15 12:38:42 +0800

    add logic to main plugin class

--- src/main/java/com/prayerflickhelper/PrayerFlickHelperPlugin.java
+++ src/main/java/com/prayerflickhelper/PrayerFlickHelperPlugin.java
@@ -7,11 +7,16 @@ import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.events.HitsplatApplied;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Slf4j
 @PluginDescriptor(
@@ -28,6 +33,8 @@ public class PrayerFlickHelperPlugin extends Plugin
 	@Inject
 	private OverlayManager overlayManager;
 
+	public static List<HitsplatApplied> hitsplatZeroesOnPlayer = new CopyOnWriteArrayList<>();
+
 	@Override
 	protected void startUp() throws Exception
 	{
@@ -40,6 +47,28 @@ public class PrayerFlickHelperPlugin extends Plugin
 	{
 		log.info("PrayerFlickHelper end");
 		overlayManager.remove(prayerFlickHelperOverlay);
+	}
+
+	@Subscribe
+	public void onHitsplatApplied(HitsplatApplied hitsplatApplied)
+	{
+		if (hitsplatApplied.getHitsplat().getAmount() != 0)
+		{
+			return;
+		}
+		if (!hitsplatApplied.getHitsplat().isMine() || !hitsplatApplied.getActor().equals(client.getLocalPlayer()))
+		{
+			return;
+		}
+		// { hitsplat of "0" was applied by an enemy on the player }
+		hitsplatZeroesOnPlayer.add(hitsplatApplied);
+	}
+
+	@Subscribe
+	public void onGameTick(GameTick gameTick)
+	{
+		int currentGameCycle = client.getGameCycle();
+		hitsplatZeroesOnPlayer.removeIf(ha -> ha.getHitsplat().getDisappearsOnGameCycle() > currentGameCycle);
 	}
 
 	@Provides
